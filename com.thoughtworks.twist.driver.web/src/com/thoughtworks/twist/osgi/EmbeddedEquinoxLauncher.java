40eb4aee0727bd703429ca4eb61222cf0bedeacf
Håkan Råberg <hraberg@yahoo.se>
2009-01-24 01:30:45 +0000

    Added the Eclipse-ExtensibleAPI: true flag so PDE detects the fragments correctly. This with a few other fixes allows the driver to be developed as a normal java project while inside Eclipse.

--- com.thoughtworks.twist.driver.web/src/com/thoughtworks/twist/osgi/EmbeddedEquinoxLauncher.java
+++ com.thoughtworks.twist.driver.web/src/com/thoughtworks/twist/osgi/EmbeddedEquinoxLauncher.java
@@ -73,7 +73,7 @@ public class EmbeddedEquinoxLauncher {
 
 		configuration = configureInstallArea(eclipseUrl);
 	}
-	
+
 	public void addProperty(String key, String value) {
 		configuration.put(key, value);
 	}
